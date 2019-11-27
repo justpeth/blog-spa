c090c8b378373d8e2c1db4229d0ad35bf42301c8
justpeth <justpeth@163.com>
2019-11-27 17:23:47 +0800

    添加公网备案号

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -9,7 +9,10 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-inner">
         <p className="copyright">Copyright© 2019 By justpeth</p>
-        <p><a className="footer-link" href="http://www.beian.miit.gov.cn/" rel="noopener noreferrer" target="_blank">蜀ICP备19027770号-1</a></p>
+        <p>
+          <a className="footer-link" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010802000779" rel="noopener noreferrer" target="_blank">蜀公网安备 51010802000779号</a>
+          <a className="footer-link" href="http://www.beian.miit.gov.cn/" rel="noopener noreferrer" target="_blank">蜀ICP备19027770号-1</a>
+        </p>
       </div>
     </div>
   )
